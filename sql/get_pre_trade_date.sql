2a4d5822bcd2e3e0136d707d271add134d06892b
Kindness <chenhongtai21@foxmail.com>
2025-08-02 18:55:03 +0800

    20250802

--- sql/get_pre_trade_date.sql
+++ sql/get_pre_trade_date.sql
@@ -1,1 +1,5 @@
-select a.*, max(case when a.is_trading_day=1 then a.trade_date else null end)over(order by a.trade_date )  pre_trade_date from  adata.trade_calendar a;
+select
+	a.*,
+	max(case when a.is_trading_day = 1 then a.trade_date else null end)over(order by a.trade_date ) pre_trade_date
+from
+	adata.trade_calendar a;
